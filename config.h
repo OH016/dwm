01152f4e22ced856da366c4052b86b3f285f2195
OH016 <oh016k@163.com>
2024-01-03 17:55:37 +0800

    新增：音量调节

--- config.h
+++ config.h
@@ -65,15 +65,19 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *xbacklight_up[] = { "xbacklight", "-inc", "5", NULL };
-static const char *xbacklight_dn[] = { "xbacklight", "-dec", "5", NULL };
+static const char *light_up[] = { "xbacklight", "-inc", "5", NULL };
+static const char *light_dn[] = { "xbacklight", "-dec", "5", NULL };
+static const char *vol_up[] = { "pactl", "set-sink-volume", "0", "+5%", NULL };
+static const char *vol_dn[] = { "pactl", "set-sink-volume", "0", "-5%", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,			XK_F12,    spawn,	   {.v = xbacklight_up } },
-	{ MODKEY,			XK_F11,	   spawn,	   {.v = xbacklight_dn } },
+	{ MODKEY,					    XK_F10,    spawn,	   	   {.v = vol_up } },
+	{ MODKEY,						XK_F9,	   spawn,	   	   {.v = vol_dn } },
+	{ MODKEY,					    XK_F12,    spawn,	   	   {.v = light_up } },
+	{ MODKEY,						XK_F11,	   spawn,	   	   {.v = light_dn } },
 	{ MODKEY,                       XK_a,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,            		XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,            			XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },

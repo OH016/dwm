0a51676b082ec963e343fc4414fd1d19b6c6b60f
OH016 <oh016k@163.com>
2024-01-04 12:29:40 +0800

    添加锁屏

--- config.h
+++ config.h
@@ -65,6 +65,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *lock[] = { "slock", NULL };
 static const char *light_up[] = { "xbacklight", "-inc", "5", NULL };
 static const char *light_dn[] = { "xbacklight", "-dec", "5", NULL };
 static const char *vol_up[] = { "pactl", "set-sink-volume", "0", "+5%", NULL };
@@ -78,6 +79,7 @@ static const Key keys[] = {
 	{ MODKEY,						XK_F11,	   spawn,	   	   {.v = light_dn } },
 	{ MODKEY,                       XK_a,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,            			XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,            			XK_F2, 	   spawn,          {.v = lock } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },

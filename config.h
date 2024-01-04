b7a318490bb5c5fdf35853694ab94071f791465e
OH016 <oh016k@163.com>
2024-01-04 15:03:15 +0800

    颜色主题

--- config.h
+++ config.h
@@ -5,7 +5,7 @@ static const unsigned int borderpx  = 4;        /* border pixel of windows */
 static const unsigned int gappx     = 12;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayonleft = 1;    /* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;        /* 0 means no systray */
@@ -13,15 +13,12 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "cascadia code:size=12",":size=12" };
 static const char dmenufont[]       = "cascadia code:size=12";
-static const char col_gray1[]       = "#131822";
-static const char col_gray2[]       = "#131822";
-static const char col_gray3[]       = "#C1E8FF";
-static const char col_gray4[]       = "#C1E8FF";
-static const char col_cyan[]        = "#131822";
-static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_gray3 },
+static const char col_bg[]       = "#131822";
+static const char col_fg[]       = "#C1E8FF";
+static const char *colors[][3]   = {
+	/*               fg      bg       border */
+	[SchemeNorm] = { col_fg, col_bg,  col_fg },
+	[SchemeSel]  = { col_fg, col_bg,  col_fg },
 };
 
 /* tagging */
@@ -63,7 +60,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *lock[] = { "slock", NULL };
 static const char *light_up[] = { "xbacklight", "-inc", "5", NULL };

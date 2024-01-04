277af5fa0710aca4af5e79528d534d8fc6273dd4
OH016 <oh016k@163.com>
2024-01-04 09:52:47 +0800

    更新脚本

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -12,19 +12,24 @@ dwm_backlight () {
 dwm_battery () {
     CHARGE=$(cat /sys/class/power_supply/BAT0/capacity)
     STATUS=$(cat /sys/class/power_supply/BAT0/status)
-    echo "BAT ${CHARGE}% [$STATUS]"
+    if [ "$STATUS" == "Not charging" ]; then
+        echo "BAT ${CHARGE}%"
+    else
+        echo "BAT ${CHARGE}% [$STATUS]"
+    fi
 }
 
 dwm_network () {
     SSID=$(iwctl station wlan0 show | awk '/Connected network/{print $3}')
-    echo "wlan0 [$SSID]"
+    echo "NET [$SSID]"
 }
 
 while true; do
-   xsetroot -name "| $(dwm_network) | $(dwm_alsa) | $(dwm_backlight) | $(dwm_battery) | $(date +"%T") "
+   xsetroot -name "| $(dwm_network) | $(dwm_alsa) | $(dwm_backlight) | $(dwm_battery) | $(date +"%R") "
    sleep 1s
 done &
 
+
 fcitx5 &
 feh --recursive --randomize --bg-fill ~/Pictures/壁纸/1.jpg &
-picom --config ~/.dwm/picom/picom.conf
+picom --experimental-backends --config ~/.dwm/picom/picom.conf

989931c09f75f40458ef3698023516ed063d5c56
OH016 <oh016k@163.com>
2024-01-07 00:17:56 +0800

    新增：截图（windows快捷键）

--- config.h
+++ config.h
@@ -63,6 +63,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *lock[] = { "slock", NULL };
+static const char *screenshot[] = { "flameshot", "gui", NULL };
 static const char *light_up[] = { "xbacklight", "-inc", "5", NULL };
 static const char *light_dn[] = { "xbacklight", "-dec", "5", NULL };
 static const char *vol_up[] = { "pactl", "set-sink-volume", "0", "+5%", NULL };
@@ -77,6 +78,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_a,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,            			XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,            			XK_F2, 	   spawn,          {.v = lock } },
+	{ MODKEY|ShiftMask,            	XK_s, 	   spawn,          {.v = screenshot } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
